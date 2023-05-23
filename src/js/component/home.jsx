0e9a49446be74eb22e05d05df7d4c3ee03da17cc
Juan Ramos <127767142+jars4u@users.noreply.github.com>
2023-05-23 22:32:36 +0000

    useState

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,18 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 
+//En React, si quiero agregar --> concat
+// 			si quiero borrar  --> filter
+// 			si quiero actualizar --> map
 
 const Home = () => {
+	const [inputValue, setInputValue] = useState ("");
+	const [todos, setTodos] = useState ([]);
+
+
+
 	return (
 		<div className="container">
 			<h1>My ToDos</h1>
 			<ul>
-				<li className="flex-container">
+				<li className="flex-container; justify-content: space-between;">
 					<input
 						type="text"
+						onChange= {(e) => setInputValue(e.target.value)}
+						value={inputValue}
+						onKeyPress  ={(e) => {
+							if (e.key === "Enter") {
+								setTodos(todos.concat(inputValue));
+							}
+						}}
 						placeholder="¿Que necesitas hacer?">
 					</input>
 				</li>
-				<li>Hacer la cama <i class="far fa-times-circle"></i></li>
+				<li>Hacer la cama <i class="far fa-times-circle "></i></li>
 				<li>Comprar comida <i class="far fa-times-circle"></i></li>
 				<li>Realizar proyectos <i class="far fa-times-circle"></i></li>
 				<li>Cita en el cine <i class="far fa-times-circle"></i></li>
